4d25d1cb0c5d750b300fe6e9f8ede1f8fe64d2cd
TapCap07 <82798353+TapCap07@users.noreply.github.com>
2021-06-10 17:35:30 +0530

    Add files via upload

---
+++ Inovexa/keywords.py
@@ -0,0 +1,35 @@
+
+
+inventions={"bulb":["imgSpch","The light bulb was invented by Thomas Edison in the 19th century","edison"], 
+             "printing press":["imgSpch","German inventor Johannes Gutenberg invents the printing press.","gutenberg"], 
+             
+             "steam engine":["imgSpch","an Englishman is usually credited with inventing the first steam engine. In 1698, Thomas Savery, an engineer and inventor, patented a machine that could effectively draw water from flooded mines using steam pressure.","thomas"],
+             "telescope":["imgSpch","the invention of the telescope is credited to Hans Lippershey, a Dutch spectacle maker. Legend has it that Lippershey got the inspiration for his design when he observed two children playing in his shop, holding up two lenses such that it made a distant weathervane appear nearby.”","hans"], 
+             
+             "vaccine":["imgSpch","Edward Jenner is considered the founder of vaccinology in the West in 1796, after he inoculated a 13 year-old-boy with vaccinia virus (cowpox), and demonstrated immunity to smallpox. In 1798, the first smallpox vaccine was developed.","jenner"],
+             "antibiotic":["imgSpch","the first true antibiotic, was discovered by Alexander Fleming, Professor of Bacteriology at St. Mary's Hospital in London","fleming"],             
+             "computer":["imgSpch","English mathematician and inventor Charles Babbage is credited with having conceived the first automatic digital computer. During the mid-1830s Babbage developed plans for the Analytical Engine.","babbage"], 
+             
+             "bell":["imgSpch","Alexander Graham Bell is often credited with being the inventor of the telephone since he was awarded the first successful patent.","bell"],
+             
+             "exit":["exit","Good Bye and Take Care friend !"],
+             "close":["exit","Have a nice Day!"]
+             
+             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
